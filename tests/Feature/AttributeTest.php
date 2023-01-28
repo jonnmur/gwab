95e59750036658a84bcb735f6e1be66d7fa68308
Joonas Murand <joonasmurand@gmail.com>
2023-01-29 01:30:38 +0200

    Add tests

---
+++ tests/Feature/AttributeTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Attribute;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AttributeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testApiGetReturnsAttributesList()
+    {
+        // Create attributes
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        $response = $this->call('GET', '/api/attributes');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'id' => $attribute1->id,
+                    'name' => $attribute1->name,
+                ],
+                [
+                    'id' => $attribute2->id,
+                    'name' => $attribute2->name,
+                ],
+            ],
+        ]);
+    }
+
+    public function testApiGetReturnsSingleAttributeById()
+    {
+        // Create attributes
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        $response = $this->call('GET', '/api/attributes/' . $attribute1->id);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'data' => [
+                'id' => $attribute1->id,
+                'name' => $attribute1->name,
+            ],
+        ]);
+    }
+
+    public function testApiGetReturns404IfAttributeDoesNotExist()
+    {
+        $response = $this->call('GET', '/api/attributes/9999');
+
+        $response->assertStatus(404);
+    }
+
+    public function testApiPostCreatesAttribute()
+    {
+        $response = $this->call('POST', '/api/attributes/', [
+            'name' => 'Testing Attribute'
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertEquals('Attribute Created', $response->json()['message']);
+        $this->assertEquals('Testing Attribute', $response->json()['data']['name']);
+    }
+
+    public function testApiPostReturns422IfValidationFails()
+    {
+        $response = $this->call('POST', '/api/attributes/', [
+            'name' => ''
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testApiPutUpdatesAttribute()
+    {
+        // Create attribute
+        $attribute = new Attribute();
+        $attribute->name = 'Testing Attribute';
+        $attribute->save();
+
+        $response = $this->call('PUT', '/api/attributes/' . $attribute->id, [
+            'name' => 'Testing Attribute Modified'
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertEquals('Attribute Updated', $response->json()['message']);
+        $this->assertEquals('Testing Attribute Modified', $response->json()['data']['name']);
+    }
+
+    public function testApiPutReturns404IfAttributeDoesNotExist()
+    {
+        $response = $this->call('PUT', '/api/attributes/9999');
+
+        $response->assertStatus(404);
+    }
+
+    public function testApiPutReturns422IfValidationFails()
+    {
+        // Create attribute
+        $attribute = new Attribute();
+        $attribute->name = 'Testing Attribute';
+        $attribute->save();
+
+        $response = $this->call('PUT', '/api/attributes/' . $attribute->id, [
+            'name' => ''
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testApiDeleteDeletesAttribute()
+    {
+        // Create attribute
+        $attribute = new Attribute();
+        $attribute->name = 'Testing Attribute';
+        $attribute->save();
+
+        $response = $this->call('DELETE', '/api/attributes/' . $attribute->id);
+
+        $response->assertStatus(200);
+
+        $this->assertEquals('Attribute Deleted', $response->json()['message']);
+        $this->assertEquals('Testing Attribute', $response->json()['data']['name']);
+    }
+
+    public function testApiDeleteReturns404IfAttributeDoesNotExist()
+    {
+        $response = $this->call('DELETE', '/api/attributes/9999');
+
+        $response->assertStatus(404);
+    }
+}
---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Attribute;
+use App\Models\Product;
+use App\Models\ProductAttribute;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testApiGetReturnsProductList()
+    {
+        // Create attributes
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        // Create products
+        $product1 = new Product();
+        $product1->name = 'Testing Product 1';
+        $product1->price = 11.99;
+        $product1->save();
+
+        $product1->attributes()->attach([$attribute1->id]);
+
+        $product2 = new Product();
+        $product2->name = 'Testing Product 2';
+        $product2->price = 12.99;
+        $product2->save();
+
+        $product2->attributes()->attach([$attribute1->id, $attribute2->id]);
+
+        $response = $this->call('GET', '/api/products');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'id' => $product1->id,
+                    'name' => $product1->name,
+                    'price' => $product1->price,
+                    'attributes' => [
+                        [
+                            'id' => $attribute1->id,
+                            'name' => $attribute1->name,
+                        ],
+                    ],
+                ],
+                [
+                    'id' => $product2->id,
+                    'name' => $product2->name,
+                    'price' => $product2->price,
+                    'attributes' => [
+                        [
+                            'id' => $attribute1->id,
+                            'name' => $attribute1->name,
+                        ],
+                        [
+                            'id' => $attribute2->id,
+                            'name' => $attribute2->name,
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testApiGetReturnsSingleProductById()
+    {
+        // Create attributes
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        // Create products
+        $product1 = new Product();
+        $product1->name = 'Testing Product 1';
+        $product1->price = 11.99;
+        $product1->save();
+
+        $product1->attributes()->attach([$attribute1->id]);
+
+        $product2 = new Product();
+        $product2->name = 'Testing Product 2';
+        $product2->price = 12.99;
+        $product2->save();
+
+        $product2->attributes()->attach([$attribute1->id, $attribute2->id]);
+
+        $response = $this->call('GET', '/api/products/' . $product1->id);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'data' => [
+                'id' => $product1->id,
+                'name' => $product1->name,
+                'price' => $product1->price,
+                'attributes' => [
+                    [
+                        'id' => $attribute1->id,
+                        'name' => $attribute1->name,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testApiGetReturns404IfProductDoesNotExist()
+    {
+        $response = $this->call('GET', '/api/products/9999');
+
+        $response->assertStatus(404);
+    }
+
+    public function testApiPostCreatesProductWithAttributesIfTheyExist()
+    {
+        // Create attributes
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'Testing Product',
+            'price' => 13.99,
+            'attributes' => [$attribute1->id, $attribute2->id],
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertEquals('Product Created', $response->json()['message']);
+        $this->assertEquals('Testing Product', $response->json()['data']['name']);
+        $this->assertEquals('Testing Attribute 1', $response->json()['data']['attributes'][0]['name']);
+        $this->assertEquals('Testing Attribute 2', $response->json()['data']['attributes'][1]['name']);
+    }
+
+    public function testApiPostCreatesProductWithoutAttributesThatDoNotExist()
+    {
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'Testing Product',
+            'price' => 13.99,
+            'attributes' => [9999],
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertEquals('Product Created', $response->json()['message']);
+        $this->assertEquals('Testing Product', $response->json()['data']['name']);
+        $this->assertEquals([], $response->json()['data']['attributes']);
+
+    }
+
+    public function testApiPostReturns422IfValidationFails()
+    {
+        $response = $this->call('POST', '/api/products/', [
+            'name' => '',
+            'price' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testApiPutUpdatesProduct()
+    {
+        // Create attribute
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $product->attributes()->attach([$attribute1->id]);
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => 'Testing Product Modified',
+            'price' => 12.99,
+            'attributes' => [$attribute2->id],
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertEquals('Product Updated', $response->json()['message']);
+        $this->assertEquals('Testing Product Modified', $response->json()['data']['name']);
+        $this->assertEquals('Testing Attribute 2', $response->json()['data']['attributes'][0]['name']);
+    }
+
+    public function testApiPutUpdatesProductWithoutAttributeThatDoesNotExist()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        // Create attribute
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $product->attributes()->attach([$attribute1->id]);
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => 'Testing Product Modified',
+            'price' => 12.99,
+            'attributes' => [9999],
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertEquals('Product Updated', $response->json()['message']);
+        $this->assertEquals('Testing Product Modified', $response->json()['data']['name']);
+        $this->assertEquals([], $response->json()['data']['attributes']);
+    }
+
+    public function testApiPutReturns404IfProductDoesNotExist()
+    {
+        $response = $this->call('PUT', '/api/products/99999');
+
+        $response->assertStatus(404);
+    }
+
+    public function testApiPutReturns422IfValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => '',
+            'price' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testApiDeleteDeletesProduct()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $response = $this->call('DELETE', '/api/products/' . $product->id);
+
+        $response->assertStatus(200);
+
+        $this->assertEquals('Product Deleted', $response->json()['message']);
+        $this->assertEquals('Testing Product', $response->json()['data']['name']);
+    }
+
+    public function testApiDeleteReturns404IfProductDoesNotExist()
+    {
+        $response = $this->call('DELETE', '/api/product/9999');
+
+        $response->assertStatus(404);
+    }
+}
