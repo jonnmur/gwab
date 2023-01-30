12bfbfc99e3b7d1ecf711b00dfdde052366481b8
Joonas Murand <joonasmurand@gmail.com>
2023-01-30 19:07:06 +0200

    Add more Attribute validation rules tests

--- tests/Feature/AttributeTest.php
+++ tests/Feature/AttributeTest.php
@@ -81,13 +81,39 @@ class AttributeTest extends TestCase
         $this->assertEquals('Testing Attribute', $response->json()['data']['name']);
     }
 
-    public function testApiPostReturns422IfValidationFails()
+    public function testApiPostReturns422IfNameRequiredValidationFails()
     {
         $response = $this->call('POST', '/api/attributes/', [
-            'name' => ''
+            'name' => '',
         ]);
 
         $response->assertStatus(422);
+        $this->assertEquals('The name field is required.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPostReturns422IfNameFieldLengthValidationFails()
+    {
+        $response = $this->call('POST', '/api/attributes/', [
+            'name' => 'upF1V5uO3O3wHzZvTzWfFfwZBR6owc3OlG3E0Xxn7SSZxyOb0mRUL3HiAbQrFnbbN8HSmKz79oICn0VhlFjUqOjunqtD7ZRp0J7RCBEcwhyo840ojONap6zDjMOobwBzpxITvgyuFgt5ylC7KZy0mTUquAYeWlmIoeX3F5WIptTSDCknDAX9dCcboFwz9X5bkJsXCOiOFXamars1pwZIHCxk0yElnV4PvxK9GcOWiRJD882RfoNhYDrVdTI70mlk',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name must not be greater than 255 characters.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPostReturns422IfNameFieldUniqueValidationFails()
+    {
+        // Create attributes
+        $attribute = new Attribute();
+        $attribute->name = 'Testing Attribute';
+        $attribute->save();
+
+        $response = $this->call('POST', '/api/attributes/', [
+            'name' => 'Testing Attribute',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name has already been taken.', $response->json()['errors']['name'][0]);
     }
 
     public function testApiPutUpdatesAttribute()
@@ -126,6 +152,42 @@ class AttributeTest extends TestCase
         ]);
 
         $response->assertStatus(422);
+
+        $this->assertEquals('The name field is required.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPutReturns422IfNameFieldLengthValidationFails()
+    {
+        // Create attribute
+        $attribute = new Attribute();
+        $attribute->name = 'Testing Attribute';
+        $attribute->save();
+
+        $response = $this->call('PUT', '/api/attributes/' . $attribute->id, [
+            'name' => 'upF1V5uO3O3wHzZvTzWfFfwZBR6owc3OlG3E0Xxn7SSZxyOb0mRUL3HiAbQrFnbbN8HSmKz79oICn0VhlFjUqOjunqtD7ZRp0J7RCBEcwhyo840ojONap6zDjMOobwBzpxITvgyuFgt5ylC7KZy0mTUquAYeWlmIoeX3F5WIptTSDCknDAX9dCcboFwz9X5bkJsXCOiOFXamars1pwZIHCxk0yElnV4PvxK9GcOWiRJD882RfoNhYDrVdTI70mlk',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name must not be greater than 255 characters.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPutReturns422IfNameFieldUniqueValidationFails()
+    {
+        // Create attributes
+        $attribute1 = new Attribute();
+        $attribute1->name = 'Testing Attribute 1';
+        $attribute1->save();
+
+        $attribute2 = new Attribute();
+        $attribute2->name = 'Testing Attribute 2';
+        $attribute2->save();
+
+        $response = $this->call('PUT', '/api/attributes/' . $attribute1->id, [
+            'name' => 'Testing Attribute 2',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name has already been taken.', $response->json()['errors']['name'][0]);
     }
 
     public function testApiDeleteDeletesAttribute()
