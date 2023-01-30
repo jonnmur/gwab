83ab0309162d0b47f166d3afd9fd0dc8b204828b
Joonas Murand <joonasmurand@gmail.com>
2023-01-30 19:22:00 +0200

    Add more product validation tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -288,14 +288,76 @@ class ProductTest extends TestCase
         $this->assertEquals([], $response->json()['data']['attributes']);
     }
 
-    public function testApiPostReturns422IfValidationFails()
+    public function testApiPostReturns422IfNameRequiredValidationFails()
     {
         $response = $this->call('POST', '/api/products/', [
             'name' => '',
-            'price' => '',
+            'price' => 10.99,
         ]);
 
         $response->assertStatus(422);
+        $this->assertEquals('The name field is required.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPostReturns422IfNameLengthValidationFails()
+    {
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'upF1V5uO3O3wHzZvTzWfFfwZBR6owc3OlG3E0Xxn7SSZxyOb0mRUL3HiAbQrFnbbN8HSmKz79oICn0VhlFjUqOjunqtD7ZRp0J7RCBEcwhyo840ojONap6zDjMOobwBzpxITvgyuFgt5ylC7KZy0mTUquAYeWlmIoeX3F5WIptTSDCknDAX9dCcboFwz9X5bkJsXCOiOFXamars1pwZIHCxk0yElnV4PvxK9GcOWiRJD882RfoNhYDrVdTI70mlk',
+            'price' => 10.99,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name must not be greater than 255 characters.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPostReturns422IfNameUniqueValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+        
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'Testing Product',
+            'price' => 10.99,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name has already been taken.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPostReturns422IfPriceNumericValidationFails()
+    {
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'Testing Product',
+            'price' => 'string',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The price must be a number.', $response->json()['errors']['price'][0]);
+    }
+
+    public function testApiPostReturns422IfPriceMinValidationFails()
+    {
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'Testing Product',
+            'price' => -100,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The price must be between 0 and 999999.99.', $response->json()['errors']['price'][0]);
+    }
+
+    public function testApiPostReturns422IfPriceMaxValidationFails()
+    {
+        $response = $this->call('POST', '/api/products/', [
+            'name' => 'Testing Product',
+            'price' => 10000000,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The price must be between 0 and 999999.99.', $response->json()['errors']['price'][0]);
     }
 
     public function testApiPutUpdatesProduct()
@@ -369,6 +431,7 @@ class ProductTest extends TestCase
         $response->assertStatus(404);
     }
 
+    /*
     public function testApiPutReturns422IfValidationFails()
     {
         // Create product
@@ -383,6 +446,115 @@ class ProductTest extends TestCase
         ]);
 
         $response->assertStatus(422);
+    }
+    */
+
+    public function testApiPutReturns422IfNameRequiredValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => '',
+            'price' => 10.99,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name field is required.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPutReturns422IfNameLengthValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => 'upF1V5uO3O3wHzZvTzWfFfwZBR6owc3OlG3E0Xxn7SSZxyOb0mRUL3HiAbQrFnbbN8HSmKz79oICn0VhlFjUqOjunqtD7ZRp0J7RCBEcwhyo840ojONap6zDjMOobwBzpxITvgyuFgt5ylC7KZy0mTUquAYeWlmIoeX3F5WIptTSDCknDAX9dCcboFwz9X5bkJsXCOiOFXamars1pwZIHCxk0yElnV4PvxK9GcOWiRJD882RfoNhYDrVdTI70mlk',
+            'price' => 10.99,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name must not be greater than 255 characters.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPutReturns422IfNameUniqueValidationFails()
+    {
+        // Create product
+        $product1 = new Product();
+        $product1->name = 'Testing Product 1';
+        $product1->price = 11.99;
+        $product1->save();
+
+        // Create product
+        $product2 = new Product();
+        $product2->name = 'Testing Product 2';
+        $product2->price = 13.99;
+        $product2->save();
+        
+        $response = $this->call('PUT', '/api/products/' . $product1->id, [
+            'name' => 'Testing Product 2',
+            'price' => 10.99,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The name has already been taken.', $response->json()['errors']['name'][0]);
+    }
+
+    public function testApiPutReturns422IfPriceNumericValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => 'Testing Product',
+            'price' => 'string',
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The price must be a number.', $response->json()['errors']['price'][0]);
+    }
+
+    public function testApiPutReturns422IfPriceMinValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => 'Testing Product',
+            'price' => -100,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The price must be between 0 and 999999.99.', $response->json()['errors']['price'][0]);
+    }
+
+    public function testApiPutReturns422IfPriceMaxValidationFails()
+    {
+        // Create product
+        $product = new Product();
+        $product->name = 'Testing Product';
+        $product->price = 11.99;
+        $product->save();
+        
+        $response = $this->call('PUT', '/api/products/' . $product->id, [
+            'name' => 'Testing Product',
+            'price' => 10000000,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertEquals('The price must be between 0 and 999999.99.', $response->json()['errors']['price'][0]);
     }
 
     public function testApiDeleteDeletesProduct()
